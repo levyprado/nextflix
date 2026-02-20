f547abaabb2cc8c6f5e76d79f85aefd876380e0d
Levy Prado <levyprado28@gmail.com>
2026-02-20 19:44:13 -0400

    feat: create nav component with active link styles

---
+++ src/components/nav.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const navItems = [
+  {
+    label: 'Home',
+    href: '/',
+  },
+  {
+    label: 'Movies',
+    href: '/movies',
+  },
+  {
+    label: 'TV Shows',
+    href: '/tv-shows',
+  },
+  {
+    label: 'New',
+    href: '/new',
+  },
+  {
+    label: 'Watchlist',
+    href: '/watchlist',
+  },
+]
+
+export default function Nav() {
+  const pathname = usePathname()
+
+  return (
+    <nav className='absolute top-1/2 left-1/2 hidden -translate-1/2 gap-6 font-bold md:flex xl:gap-8'>
+      {navItems.map(({ href, label }) => {
+        const isActive = pathname === href
+
+        return (
+          <Link
+            key={href}
+            href={href}
+            className={cn(
+              'relative after:absolute after:-bottom-1 after:left-1/2 after:h-0.5 after:w-0 after:-translate-x-1/2 after:rounded-full after:bg-foreground after:transition-[width] after:duration-200 hover:after:w-full',
+              isActive && 'after:w-full',
+            )}
+          >
+            {label}
+          </Link>
+        )
+      })}
+    </nav>
+  )
+}
