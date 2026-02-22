e1fa3b7e3a63800786fd1b922eebb54b83d4d6ad
Levy Prado <levyprado28@gmail.com>
2026-02-21 23:55:38 -0400

    style: tweak logo visibility breakpoints for sidebar compatibility

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <Link href='/' className='flex items-center gap-2'>
       <span className='size-0 border-t-30 border-r-18 border-l-18 border-t-amber-600 border-r-transparent border-l-transparent' />
-      <span className='hidden font-heading text-2xl font-extrabold sm:block md:hidden lg:block'>
+      <span className='hidden font-heading text-2xl font-extrabold sm:block md:hidden xl:block'>
         Nextflix
       </span>
     </Link>
