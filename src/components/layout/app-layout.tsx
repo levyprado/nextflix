d4e61c93290ca7e2d702aa73c16506d8fe31568d
Levy Prado <levyprado28@gmail.com>
2026-02-23 23:44:06 -0400

    refactor: update app layout and header to use new sidebar width

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -9,12 +9,12 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div className='flex min-h-screen w-full'>
+    <div className='flex min-h-svh w-full'>
       <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
       <div
         className={cn(
           'flex flex-1 flex-col overflow-x-auto transition-[padding] duration-450 ease-[cubic-bezier(0.32,0.72,0,1)]',
-          isOpen ? 'lg:pl-[300px]' : 'pl-0',
+          isOpen ? 'lg:pl-[280px]' : 'pl-0',
         )}
       >
         <Header isOpen={isOpen} setIsOpen={setIsOpen} />
