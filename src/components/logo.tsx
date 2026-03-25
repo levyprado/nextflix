9161315de58285b8fe08e34a79591d434ae6dd35
Levy Prado <levyprado28@gmail.com>
2026-03-24 21:50:53 -0400

    style: update logo color to foreground

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <Link
       href='/'
-      className='size-0 border-t-32 border-r-[19.2px] border-l-[19.2px] border-t-amber-600 border-r-transparent border-l-transparent transition-transform duration-450 ease-[cubic-bezier(0.32,0.72,0,1)] hover:-rotate-180'
+      className='size-0 border-t-32 border-r-[19.2px] border-l-[19.2px] border-foreground border-r-transparent border-l-transparent transition-transform duration-450 ease-[cubic-bezier(0.32,0.72,0,1)] hover:-rotate-180'
     ></Link>
   )
 }
