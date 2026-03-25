310d5d6a65588b7a9dc2cda7062122aa842ea234
Levy Prado <levyprado28@gmail.com>
2026-03-24 22:50:01 -0400

    refactor: componentize movie detail page

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -1,4 +1,6 @@
 import Container from '@/components/layout/container'
+import MediaCard from '@/components/media/media-card'
+import ScrollSection from '@/components/media/scroll-section'
 import { Button } from '@/components/ui/button'
 import Icon from '@/components/ui/icon'
 import {
@@ -22,7 +24,6 @@ export default async function MovieDetailPage({
 
   return (
     <div className='min-h-screen pb-16'>
-      {/* Background cover */}
       <div className='relative h-[50svh] w-full lg:h-[70vh]'>
         <div className='absolute inset-0 -z-10'>
           <Image
@@ -113,111 +114,59 @@ export default async function MovieDetailPage({
             </div>
           </div>
         </section>
-        <section className='flex flex-col gap-4 md:gap-6 lg:gap-8'>
-          <h2 className='font-heading text-xl font-bold md:text-2xl lg:text-3xl'>
-            Top Cast
-          </h2>
-          <ul className='no-scrollbar flex gap-4 overflow-x-auto'>
-            {[...Array(12).keys()].map((i) => (
-              <li key={i}>
-                <Link
-                  href='/actor/123'
-                  className='flex w-24 flex-col gap-2.5 md:w-32 lg:w-40'
-                >
-                  <div className='relative aspect-square overflow-hidden rounded-full'>
-                    <Image
-                      src='https://image.tmdb.org/t/p/w342/xcCv7C1zNuIr3JeNau9UjZLaVC1.jpg'
-                      alt={'Actor name'}
-                      fill
-                      className='object-cover object-[70%_30%]'
-                    />
-                  </div>
-                  <div className='space-y-0.5 text-center text-xs md:text-sm lg:text-base'>
-                    <p className='truncate leading-tight font-semibold text-foreground/90'>
-                      Chris Pratt
-                    </p>
-                    <p className='truncate leading-tight text-foreground/60'>
-                      Judge Maddox
-                    </p>
-                  </div>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </section>
-        <section className='flex flex-col gap-4 md:gap-6 lg:gap-8'>
-          <h2 className='font-heading text-xl font-bold md:text-2xl lg:text-3xl'>
-            Trailers & Videos
-          </h2>
-          <ul className='no-scrollbar flex gap-4 overflow-x-auto'>
-            {[...Array(6).keys()].map((i) => (
-              <li key={i}>
-                <button className='group flex w-64 flex-col gap-1.5'>
-                  <div className='relative aspect-video overflow-hidden rounded-xl bg-foreground/15'>
-                    <div className='absolute inset-0 flex items-center justify-center bg-background/30 transition-colors group-hover:bg-background/20'>
-                      <div className='absolute top-1/2 left-1/2 flex size-12 -translate-1/2 items-center justify-center rounded-full bg-foreground/20 backdrop-blur-md transition-colors group-hover:bg-foreground/30'>
-                        <Icon icon={PlayIcon} className='fill-foreground' />
-                      </div>
-                    </div>
-                  </div>
-                  <p className='truncate text-start text-sm font-medium text-foreground/90 transition-colors group-hover:text-foreground'>
-                    Trailer {i}
-                  </p>
-                </button>
-              </li>
-            ))}
-          </ul>
-        </section>
-        <section className='flex flex-col gap-4 md:gap-6 lg:gap-8'>
-          <h2 className='font-heading text-xl font-bold md:text-2xl lg:text-3xl'>
-            You Might Also Like
-          </h2>
-          <ul className='no-scrollbar flex gap-4 overflow-x-auto'>
-            {[...Array(6).keys()].map((i) => (
-              <li
-                key={i}
-                className='group w-36 shrink-0 md:w-44 xl:w-52 2xl:w-56'
+
+        <ScrollSection title='Top Cast'>
+          {[...Array(12).keys()].map((i) => (
+            <li key={i}>
+              <Link
+                href='/actor/123'
+                className='group flex w-24 flex-col gap-2.5 md:w-32 lg:w-40'
               >
-                <Link href='/' className='flex w-full flex-col gap-2'>
-                  <div className='relative aspect-2/3 w-full overflow-hidden rounded-xl'>
-                    <Image
-                      src='https://image.tmdb.org/t/p/original/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg'
-                      alt='Poster'
-                      width={1400}
-                      height={2100}
-                      className='size-full object-cover transition-[scale] duration-350 group-hover:scale-107'
-                    />
-                    <div>
-                      <div className='absolute top-2 right-2 flex items-center gap-1 rounded-full bg-background/60 px-2 py-0.5 backdrop-blur-md'>
-                        <svg
-                          xmlns='http://www.w3.org/2000/svg'
-                          width='10'
-                          height='10'
-                          viewBox='0 0 24 24'
-                          fill='none'
-                          stroke='currentColor'
-                          strokeWidth='2'
-                          strokeLinecap='round'
-                          strokeLinejoin='round'
-                          className='fill-yellow-400 text-yellow-400'
-                        >
-                          <polygon points='12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2'></polygon>
-                        </svg>
-                        <span className='text-[10px] font-semibold md:text-xs'>
-                          7.2
-                        </span>
-                      </div>
-                    </div>
-                    <div className='absolute inset-0 bg-linear-to-t from-background/80 via-transparent to-transparent opacity-0 transition-opacity duration-350 group-hover:opacity-100' />
-                  </div>
-                  <p className='truncate leading-tight font-semibold md:text-lg'>
-                    Fight Club
+                <div className='relative aspect-square overflow-hidden rounded-full'>
+                  <Image
+                    src='https://image.tmdb.org/t/p/w342/xcCv7C1zNuIr3JeNau9UjZLaVC1.jpg'
+                    alt={'Actor name'}
+                    fill
+                    className='object-cover object-[70%_30%] transition-[scale] group-hover:scale-107'
+                  />
+                </div>
+                <div className='space-y-0.5 text-center text-xs md:text-sm lg:text-base'>
+                  <p className='truncate leading-tight font-semibold text-foreground/90'>
+                    Chris Pratt
                   </p>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </section>
+                  <p className='truncate leading-tight text-foreground/60'>
+                    Judge Maddox
+                  </p>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ScrollSection>
+        <ScrollSection title='Trailers & Videos'>
+          {[...Array(6).keys()].map((i) => (
+            <li key={i}>
+              <button className='group flex w-64 flex-col gap-1.5'>
+                <div className='relative aspect-video overflow-hidden rounded-xl bg-foreground/15'>
+                  <div className='absolute inset-0 flex items-center justify-center bg-background/30 transition-colors group-hover:bg-background/20'>
+                    <div className='absolute top-1/2 left-1/2 flex size-12 -translate-1/2 items-center justify-center rounded-full bg-foreground/20 backdrop-blur-md transition-colors group-hover:bg-foreground/30'>
+                      <Icon icon={PlayIcon} className='fill-foreground' />
+                    </div>
+                  </div>
+                </div>
+                <p className='truncate text-start text-sm font-medium text-foreground/90 transition-colors group-hover:text-foreground'>
+                  Trailer {i}
+                </p>
+              </button>
+            </li>
+          ))}
+        </ScrollSection>
+        <ScrollSection title='You Might Also Like'>
+          {[...Array(6).keys()].map((i) => (
+            <li key={i}>
+              <MediaCard />
+            </li>
+          ))}
+        </ScrollSection>
       </Container>
     </div>
   )
