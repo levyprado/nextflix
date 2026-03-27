665af701ed4b0ca7d1e8040397c7dc9c57a1041b
Levy Prado <levyprado28@gmail.com>
2026-03-27 00:01:35 -0400

    refactor: update Logo component to use ROUTES constant

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,9 +1,10 @@
+import { ROUTES } from '@/lib/routes'
 import Link from 'next/link'
 
 export default function Logo() {
   return (
     <Link
-      href='/'
+      href={ROUTES.HOME}
       className='size-0 border-t-32 border-r-[19.2px] border-l-[19.2px] border-foreground border-r-transparent border-l-transparent transition-transform duration-450 ease-[cubic-bezier(0.32,0.72,0,1)] hover:-rotate-180'
     ></Link>
   )
