a40d738f36c41fc2ed05cc74bd380b0f448fbfb8
Levy Prado <levyprado28@gmail.com>
2026-03-26 23:35:25 -0400

    feat: fetch and display real TMDB api data in home page sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,38 @@ import Container from '@/components/layout/container'
 import HeroBanner from '@/components/media/hero-banner'
 import MediaCard from '@/components/media/media-card'
 import ScrollSection from '@/components/media/scroll-section'
+import {
+  getNowPlayingMovies,
+  getPopularMovies,
+  getTopRatedMovies,
+  getUpcomingMovies,
+} from '@/lib/tmdb/api'
 
-export default function HomePage() {
+export default async function HomePage() {
+  const [nowPlaying, popular, topRated, upcoming] = await Promise.all([
+    getNowPlayingMovies(),
+    getPopularMovies(),
+    getTopRatedMovies(),
+    getUpcomingMovies(),
+  ])
+
+  const sections = [
+    { title: 'Now Playing', items: nowPlaying.results },
+    { title: 'Popular Movies', items: popular.results },
+    { title: 'Top Rated Movies', items: topRated.results },
+    { title: 'Upcoming Movies', items: upcoming.results },
+  ]
+
   return (
     <>
       <HeroBanner />
 
       <Container as='section' className='-mt-12 flex flex-col gap-16 pb-16'>
-        {[...Array(6).keys()].map((i) => (
-          <ScrollSection title='For You' key={i}>
-            {[...Array(10).keys()].map((i) => (
-              <li key={i}>
-                <MediaCard />
+        {sections.map((section) => (
+          <ScrollSection key={section.title} title={section.title}>
+            {section.items.map((item) => (
+              <li key={item.id}>
+                <MediaCard media={item} />
               </li>
             ))}
           </ScrollSection>
