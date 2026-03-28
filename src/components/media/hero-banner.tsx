e197639250615bd18333e046d6d513c2a5fc3375
Levy Prado <levyprado28@gmail.com>
2026-03-27 23:21:53 -0400

    refactor: update HeroBanner to accept dynamic media props and use Image fill

--- src/components/media/hero-banner.tsx
+++ src/components/media/hero-banner.tsx
@@ -1,42 +1,66 @@
+import { ROUTES } from '@/lib/routes'
+import type { Movie, TVShow } from '@/lib/tmdb/types'
 import { InformationCircleIcon, PlayIcon } from '@hugeicons/core-free-icons'
 import Image from 'next/image'
+import Link from 'next/link'
 import Container from '../layout/container'
-import { Button } from '../ui/button'
+import { buttonVariants } from '../ui/button'
 import Icon from '../ui/icon'
 
-export default function HeroBanner() {
+type HeroBannerProps = {
+  media: Movie | TVShow
+}
+
+export default function HeroBanner({ media }: HeroBannerProps) {
+  const isMovie = 'title' in media
+  const title = isMovie ? media.title : media.name
+  const imageUrl = `https://image.tmdb.org/t/p/original${media.backdrop_path}`
+
   return (
     <section className='relative h-[90svh] w-full'>
       <div className='absolute inset-0 -z-10'>
         <Image
-          src='https://image.tmdb.org/t/p/original/7HKpc11uQfxnw0Y8tRUYn1fsKqE.jpg'
-          alt='Backdrop'
-          width={2560}
-          height={1440}
-          className='size-full object-cover'
+          src={imageUrl}
+          alt={`${title} Backdrop`}
+          fill
+          priority
+          sizes='100vw'
+          className='object-cover'
         />
         <div className='absolute inset-0 bg-linear-to-r from-background via-transparent to-transparent' />
         <div className='absolute inset-0 bg-linear-to-t from-background via-transparent to-transparent' />
       </div>
 
       <Container className='absolute bottom-[20%] flex max-w-3xl flex-col gap-2'>
-        <h1 className='font-heading text-4xl leading-tight font-bold text-balance wrap-break-word uppercase italic lg:text-6xl lg:leading-none'>
-          Mercy
+        <h1 className='font-heading text-4xl leading-none font-bold text-balance wrap-break-word uppercase italic lg:text-6xl'>
+          {title}
         </h1>
         <p className='line-clamp-3 max-w-xl leading-relaxed text-foreground/75 lg:text-lg'>
-          In the near future, a detective stands on trial accused of murdering
-          his wife. He has ninety minutes to prove his innocence to the advanced
-          AI Judge he once championed, before it determines his fate.
+          {media.overview}
         </p>
         <div className='mt-3 flex flex-wrap gap-2.5'>
-          <Button>
+          <Link
+            href={
+              isMovie
+                ? ROUTES.MOVIE_DETAIL(media.id)
+                : ROUTES.TV_SHOW_DETAIL(media.id)
+            }
+            className={buttonVariants()}
+          >
             <Icon icon={PlayIcon} className='fill-background' />
             Watch Now
-          </Button>
-          <Button variant='secondary'>
+          </Link>
+          <Link
+            href={
+              isMovie
+                ? ROUTES.MOVIE_DETAIL(media.id)
+                : ROUTES.TV_SHOW_DETAIL(media.id)
+            }
+            className={buttonVariants({ variant: 'secondary' })}
+          >
             <Icon icon={InformationCircleIcon} />
             More Info
-          </Button>
+          </Link>
         </div>
       </Container>
     </section>
