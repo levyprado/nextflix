24fe897d4d9c529358f6fb733623f4dd69e55702
Levy Prado <levyprado28@gmail.com>
2026-04-10 00:10:11 -0400

    refactor: update BackButton layout to use container queries for showing text only when enough space available

--- src/components/back-button.tsx
+++ src/components/back-button.tsx
@@ -19,15 +19,16 @@ export default function BackButton() {
   }
 
   return (
-    <div className='px-4 md:px-8 lg:px-4'>
+    <div className='fixed top-20 z-10 px-4 md:px-8 lg:px-4 xl:top-28'>
       <Button
         variant='secondary'
         size='icon'
         onClick={handleClick}
-        className='fixed top-20 z-10 xl:top-28 2xl:h-11 2xl:w-auto 2xl:gap-2 2xl:px-6 2xl:pl-5'
+        aria-label='Go back'
+        className='@8xl:h-11 @8xl:w-auto @8xl:gap-2 @8xl:px-6 @8xl:pl-5'
       >
-        <Icon icon={ArrowLeftIcon} className='-ml-0.5 2xl:-ml-1' />
-        <span className='hidden 2xl:inline-block'>Back</span>
+        <Icon icon={ArrowLeftIcon} className='-ml-0.5 @8xl:-ml-1' />
+        <span className='sr-only @8xl:not-sr-only'>Back</span>
       </Button>
     </div>
   )
